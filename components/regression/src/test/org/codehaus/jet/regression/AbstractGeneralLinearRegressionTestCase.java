1cdb8509bc7bd817a0e7a1261d9d0366a49f0d16
mauro <mauro@4f5a9d1a-c116-0410-8a32-bdeaa3774543>
2006-06-27 17:40:43 +0000

    Refactored general linera regression tests to extend an abstract test case.

---
+++ components/regression/src/test/org/codehaus/jet/regression/AbstractGeneralLinearRegressionTestCase.java
@@ -0,0 +1,44 @@
+package org.codehaus.jet.regression;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @author Mauro Talevi
+ */
+public abstract class AbstractGeneralLinearRegressionTestCase extends TestCase {
+
+    private GeneralLinearRegression regression;
+
+    public void setUp(){
+        regression = createRegression();
+    }
+
+    protected abstract GeneralLinearRegression createRegression();
+    
+    protected abstract int getNumberOfRegressors();
+    
+    protected abstract int getSampleSize();
+
+    public void testCanCalculateBeta(){
+        double[] beta = regression.getBeta();        
+        assertEquals(getNumberOfRegressors(), beta.length);
+    }
+
+    public void testCanCalculateResiduals(){
+        double[] e = regression.getResiduals();
+        assertEquals(getSampleSize(), e.length);
+    }
+    
+    public void testCanCalculateBetaVariance(){
+        double[][] variance = regression.getBetaVariance();
+        assertEquals(getNumberOfRegressors(), variance.length);
+    }
+
+    public void testCanCalculateYVariance(){
+        double variance = regression.getYVariance();
+        assertTrue(variance > 0.0);
+    }
+    
+
+}
