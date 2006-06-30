ff5023a60a8b7e35a90d4e1db152325ed9510481
mauro <mauro@4f5a9d1a-c116-0410-8a32-bdeaa3774543>
2006-06-30 13:42:15 +0000

    Completed test coverage on engine

--- components/engine/src/test/org/codehaus/jet/engines/DefaultJetEngineTest.java
+++ components/engine/src/test/org/codehaus/jet/engines/DefaultJetEngineTest.java
@@ -1,10 +1,12 @@
-package org.codehaus.jet;
+package org.codehaus.jet.engines;
 
 import java.util.Arrays;
 
-import org.codehaus.jet.engines.DefaultJetEngine;
+import org.codehaus.jet.JetEngine;
+import org.codehaus.jet.JetException;
 import org.codehaus.jet.hypothesis.HypothesisException;
 import org.codehaus.jet.hypothesis.HypothesisTester;
+import org.codehaus.jet.hypothesis.testers.DefaultHypothesisTester;
 import org.jmock.Mock;
 import org.jmock.MockObjectTestCase;
 
@@ -39,14 +41,34 @@ public class DefaultJetEngineTest extends MockObjectTestCase {
         return (HypothesisTester)mock.proxy();
     }
 
-    public void testTesterMethodsCanBeInvoked() throws Exception {
+    public void testDefaultTesterMethodsCanBeInvoked() throws Exception {
+        engine = new DefaultJetEngine(); 
+        assertCriticalValue(DefaultHypothesisTester.URC_TEST, 1, 0, 1, 0.01, 0);
+        assertPValue(DefaultHypothesisTester.URC_TEST, 1, 0, 1, 0.01, 0);
+        assertTrue(engine.getTestNames().length > 0);
+    }
+    
+
+    private void assertCriticalValue(String testName, int integratedVariables, int regressionVariables, 
+            int testType, double level, int sampleSize) throws Exception{
+        assertTrue(engine.estimateCriticalValue(testName, new int[]{integratedVariables, regressionVariables,
+                testType}, level, sampleSize) != 0.0);
+    }
+
+    private void assertPValue(String testName, int integratedVariables, int regressionVariables, 
+            int testType, double level, int sampleSize) throws Exception{
+        assertTrue(engine.estimatePValue(testName, new int[]{integratedVariables, regressionVariables,
+                testType}, level, sampleSize) != 0.0);
+    }    
+
+    public void testCustomTesterMethodsCanBeInvoked() throws Exception {
         engine = new DefaultJetEngine(mockHypothesisTester(false)); 
         assertEquals(criticalValue, engine.estimateCriticalValue(null, null, 0.1, 100), precision);
         assertEquals(pValue, engine.estimatePValue(null, null, 0.1, 100), precision);
         assertEquals(Arrays.toString(testNames), Arrays.toString(engine.getTestNames()));
     }
 
-    public void testTesterExceptionsCanBeCaught() throws Exception {
+    public void testCustomTesterExceptionsCanBeCaught() throws Exception {
         engine = new DefaultJetEngine(mockHypothesisTester(true)); 
         try {
             assertEquals(criticalValue, engine.estimateCriticalValue(null,
